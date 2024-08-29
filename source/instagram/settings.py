6f04213a3aeb4ff287cee66f8be036381b6f9645
Dasha <grace-kramm666@gmail.com>
2024-08-30 00:15:47 +0600

    исправила ошибку

--- source/instagram/settings.py
+++ source/instagram/settings.py
@@ -142,6 +142,9 @@ MEDIA_ROOT = Path.joinpath(BASE_DIR, 'uploads')
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
